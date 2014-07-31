6f3acad7ed7435fea206ff13e8e56689dbb424b5
Nick Timkovich <npt@u.northwestern.edu>
2014-07-31 14:01:49 -0500

    Rework settings to allow dotted names

--- multiworm/conf/__init__.py
+++ multiworm/conf/__init__.py
@@ -12,37 +12,7 @@ import six
 from six.moves import (zip, filter, map, reduce, input, range)
 
 import os
-import imp
+import importlib
 
 from . import defaults
-
-local_settings = os.environ.get('MULTIWORM_SETTINGS')
-
-REQUIRE_SETTINGS_MODULE = False
-
-class Settings(object):
-    def __init__(self, settings_module):
-        # copy the global settings into the object
-        for setting in dir(defaults):
-            if setting.isupper():
-                setattr(self, setting, getattr(defaults, setting))
-
-        self.SETTINGS_MODULE = settings_module
-
-        if REQUIRE_SETTINGS_MODULE:
-            if settings_module is None:
-                raise EnvironmentError("MULTIWORM_SETTINGS not specified")
-
-        if settings_module:
-            try:
-                file, pathname, desc = imp.find_module(settings_module)
-                local_settings = imp.load_module(settings_module, file, pathname, desc)
-            except ImportError:
-                raise ImportError("Failed to load settings module: {}".format(settings_module))
-
-            for setting in dir(local_settings):
-                if setting.isupper():
-                    setattr(self, setting, getattr(local_settings, setting))
-
-
-settings = Settings(local_settings)
+from .settings import settings
